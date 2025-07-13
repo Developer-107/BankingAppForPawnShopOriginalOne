a4e3c4d0d8625b87cf4773c94f2530d61d321ab6
Your Name <you@example.com>
2025-07-13 10:42:46 +0400

    Commiting changes 1.17.3.1

--- active_contracts_window.py
+++ active_contracts_window.py
@@ -164,13 +164,13 @@ class ActiveContracts(QWidget):
         box3.setStyleSheet("QGroupBox {font-style: italic; font-size: 10pt; }")
         box_layout3 = QGridLayout()
 
-
         self.from_date = QDateEdit()
         self.from_date.setCalendarPopup(True)
-        self.from_date.setDate(QDate.currentDate().addMonths(-1))
+        self.from_date.setDate(QDate.currentDate())  # Today
+
         self.to_date = QDateEdit()
         self.to_date.setCalendarPopup(True)
-        self.to_date.setDate(QDate.currentDate())
+        self.to_date.setDate(QDate.currentDate().addDays(1))  # Tomorrow
         #
         box_layout3.addWidget(QLabel("*დან თარიღი:"))
         box_layout3.addWidget(self.from_date)
